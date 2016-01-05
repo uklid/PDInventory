f62a3e3ae7436a561626c5b293682033f3376019
Uklid Yeesarapat <uklid.y@hotmail.com>
2016-01-05 23:20:25 +0700

    add metro

---
+++ PDInventory/MainForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace PDInventory
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.lnkClose = new MetroFramework.Controls.MetroLink();
+            this.SuspendLayout();
+            // 
+            // lnkClose
+            // 
+            this.lnkClose.Image = ((System.Drawing.Image)(resources.GetObject("lnkClose.Image")));
+            this.lnkClose.ImageSize = 50;
+            this.lnkClose.Location = new System.Drawing.Point(1040, 63);
+            this.lnkClose.Name = "lnkClose";
+            this.lnkClose.Size = new System.Drawing.Size(35, 36);
+            this.lnkClose.TabIndex = 0;
+            this.lnkClose.UseSelectable = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1280, 783);
+            this.Controls.Add(this.lnkClose);
+            this.Name = "MainForm";
+            this.Text = "LogIn";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroLink lnkClose;
+    }
+}
+
